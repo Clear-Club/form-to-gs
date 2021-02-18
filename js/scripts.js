959588f5926fbc222844e941454f9264ed66a351
Miguel Menjivar <menjivarmiguel221@gmail.com>
2021-02-17 23:18:28 -0800

    tweaked the program a bit, ran into a bug that it wont reset page after submission when it did work earlier but now it doesnt

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwLCvbUAk4H7YkH43md1OfGbZ8RDSPWDYkOGDYSxAWV03GGFGNp/exec'
 const form = document.forms['submit-to-google-sheets']
 
+
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => console.log('Success!', response))
         .catch(error => console.error('Error!', error.message));
 
-    document.getElementById('form-to-google').reset();
+    // resets form after submission 
+    document.getElementById('submit-to-google-sheets').reset();
 })
+
+// using enter/return to go to next field
+// $('form input').keydown(function (e) {
+//     if (e.keyCode == 13) {
+//         var inputs = $(this).parents("form").eq(0).find(":input");
+//         if (inputs[inputs.index(this) + 1] != null) {
+//             inputs[inputs.index(this) + 1].focus();
+//         }
+//         e.preventDefault();
+//         return false;
+//     }
+// });
