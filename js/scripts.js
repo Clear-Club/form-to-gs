fec31115267c8fcb295115bef5eca7f398d9e421
Miguel Menjivar <menjivarmiguel221@gmail.com>
2021-03-13 20:22:51 -0800

    split up all form names to individual names, and added them to script file, will be running tests, also need to look into disabling return for input tags, it doesnt work at the moment

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,54 @@
 // google sheets link
-const scriptURL = '';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxNfN_PG2v5N-uQD1nLf0oMzO3j_UDUx0MUjCz0d_A3G6UZItvSmiTgp2FWOQVRUjYwEA/exec';
 
-// const form = document.forms['submit-to-google-sheets']
-const form = document.getElementById('submit-to-google-sheets');
+const form01 = document.forms['tech-action-form'];
+const form02 = document.forms['tray-tracking-form'];
+const form03 = document.forms['quality-thermo-form'];
+const form04 = document.forms['fixed-rethermo-form'];
 
-
-form.addEventListener('submit', (e) => {
+// form 01 for tech actions (index) submission
+form01.addEventListener('submit', (e) => {
     e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    fetch(scriptURL, { method: 'POST', body: new FormData(form01) })
         .then(response => console.log('Success!', response))
         .catch(error => console.error('Error!', error.message));
 
     // resets form after submission 
-    form.reset();
-})
+    form01.reset();
+});
+
+// form 02 for tray tracking submission
+form02.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form02) })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message));
+
+    // resets form after submission 
+    form02.reset();
+});
+
+// form 03 for Quality Assurance and Thermoforming
+form03.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form03) })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message));
+
+    // resets form after submission 
+    form03.reset();
+});
+
+// form 02 for tray tracking submission
+form04.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form04) })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message));
+
+    // resets form after submission 
+    form04.reset();
+});
 
 // handler for .ready()
 $(function() {
